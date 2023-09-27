fa8b0babf34aa4d41fd47e4afb0e3c2448c71d1e
ignabisi <ignacio.bisi@gmail.com>
2023-09-27 12:13:36 +0100

    Initialize basic application configuration

---
+++ app/config.py
@@ -0,0 +1,19 @@
+import logging
+from functools import lru_cache
+
+from pydantic import BaseSettings
+
+log = logging.getLogger("uvicorn")
+
+
+class Settings(BaseSettings):
+    environment: str = "dev"
+    # bool(0) = False
+    # bool(1) = True
+    testing: bool = bool(0)
+
+
+@lru_cache()
+def get_settings() -> BaseSettings:
+    log.info("Loading config settings from the environment...")
+    return Settings()
